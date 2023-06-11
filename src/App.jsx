d620f8b9f7a4d6259e5922fb2ce77f686ffd3653
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-06-11 12:29:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+       <button className="btn">Button</button>
+ 
+    </>
+  )
+}
+
+export default App
---
+++ src/Home/Home/Benner.jsx
@@ -0,0 +1,35 @@
+ 
+import { Carousel } from 'react-responsive-carousel';
+
+const Benner = () => {
+        return (
+                <div>
+                        {/* <Carousel>
+                <div>
+                    <img src={slider1} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={slider2} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={slider3} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={slider4} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={slider5} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                 
+                 
+            </Carousel> */}
+                </div>
+        );
+};
+
+export default Benner;
