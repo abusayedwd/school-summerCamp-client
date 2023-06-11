cfe44316b81a9d7a17f02a2cd32a9211686cfaf1
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-06-11 12:50:27 +0600

    layout setup and navbar

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Benner from './Home/Benner';
+
+const Home = () => {
+        return (
+                <div>
+                        <Benner></Benner>
+                </div>
+        );
+};
+
+export default Home;
--- src/Home/Home/Benner.jsx
+++ src/Home/Home/Benner.jsx
@@ -1,10 +1,15 @@
- 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
+import slider1 from '../../../public/slider1.avif'
+import slider2 from '../../../public/slider5.avif'
+import slider3 from '../../../public/slider2.jpg'
+import slider4 from '../../../public/slider3.webp'
+import slider5 from '../../../public/slider4.avif'
 
 const Benner = () => {
         return (
                 <div>
-                        {/* <Carousel>
+                        <Carousel>
                 <div>
                     <img src={slider1} />
                     <p className="legend">Legend 1</p>
@@ -27,7 +32,7 @@ const Benner = () => {
                 </div>
                  
                  
-            </Carousel> */}
+            </Carousel>
                 </div>
         );
 };
